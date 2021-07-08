2ec38880a27f791e861e6e3d4207ba7f52b9fb5c
ashtonkumar <85140494+ashtonkumar@users.noreply.github.com>
2021-07-08 14:58:11 +1200

    Add files via upload

---
+++ football quiz (python).py
@@ -0,0 +1,97 @@
+print ("this is a 5 question quiz about soccer/football whatever you like to call it")
+
+
+
+answer_1 = "a"
+question_1 = input("""who is the greatest football player of all time?
+                   A-pele
+                   B-Cristiano Ronaldo
+                   C-Lionel Messi
+                   D-Diego Maradona
+                   """)
+print("")
+
+if question_1 == answer_1:
+    print ("correct")
+    print("2 points")
+else: 
+    print ("Incorrect")
+    print("the correct answer was A Pele")
+
+
+answer_2 = "a" or "A"
+question_2 = input("""what team has won the most champions league so far?
+                   A-Real Madrid
+                   B-AC Milan
+                   C-Bayern Munich
+                   D-FC Barcelona
+                   """)
+
+if question_2 == answer_2: 
+    print ("correct")
+    print("3 points")
+else: 
+    print ("Incorrect")
+    print("the correct answer was A Real Madrid")
+    
+
+
+answer_3 = "c" or "C"
+question_3 = input("""what team has the mighty Cristiano Ronaldo in?
+                   A-Manchester United
+                   B-Real Madrid
+                   C-Juventus
+                   D-Everton
+                   """)
+
+if question_3 == answer_3: 
+    print ("correct")
+    print("3 points")
+else: 
+    print ("Incorrect")
+    print("the correct answer was C Juventus")
+
+
+    
+
+answer_4 = "b" or "B"
+question_4 = input("""who scored 5 goals in 9 minutes in the match against Wolfsburg?
+                   A-Marco Reus
+                   B-Robert Lewandowski
+                   C-Hakimi Ziyech
+                   D-Neymar JNR
+                   """)
+if question_3 == answer_3: 
+    print ("correct")
+    print("4 points")
+else: 
+    print ("Incorrect")
+    print("the correct answer was B Robert Lewandowski")
+
+
+
+    answer_5 = "d" or "D"
+question_5 = input("""What does two yellow card in a game of football mean?
+                   A-warning
+                   B-go to jail
+                   C-20 push ups
+                   D-sent off
+                   """)
+if question_4 == answer_4: 
+    print ("correct")
+    print("5 points")
+else: 
+    print ("Incorrect")
+    print("the correct answer was D sent off")
+
+
+    
+    print("thank you for trying out this short quiz add up all your points and see what you get")
+
+
+    
+
+
+
+
+   
